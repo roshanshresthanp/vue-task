253595c677b9ad3cd47d4bd2251e7219bc7b9fd7
roshanshresthanp <stha901@gmail.com>
2022-11-12 17:56:03 +0545

    Chnage

--- resources/js/components/employee.vue
+++ resources/js/components/employee.vue
@@ -3,7 +3,7 @@
 
       <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
+  Add Employee
 </button>
 
 <!-- Modal -->
